f0c4d93c667986a545d2dccaf8c62f887d34b27a
inverse231 <3xinverse@gmail.com>
2019-10-13 13:30:07 +0300

    fixed burger menu + services h3 margin

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 let header = document.querySelector('.header');
 let burger = document.getElementById('burger');
 
-burger.addEventListener('click', event => header.classList.add('header__add'));
+
+burger.addEventListener('change', event => {
+    if(burger.checked) {
+        header.classList.add('header__add');
+    }
+    else {
+        header.classList.remove('header__add');
+    }
+})
